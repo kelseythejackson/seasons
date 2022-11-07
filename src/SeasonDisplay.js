17417b7e425493607cf29406333181b7fa21c1f2
Kelsey Jackson <kelseythej@gmail.com>
2022-11-07 07:27:07 -0600

    initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SeasonDisplay = ({ lat }) => {
+  console.log(lat);
+  return <div>Season Display</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: '',
+  };
+
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading!</div>;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+/* 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +53,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+*/
