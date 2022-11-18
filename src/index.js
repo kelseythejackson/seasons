4edef33c2d776de5658a070f8517981d42f1c3a8
Kelsey Jackson <kelseythej@gmail.com>
2022-11-18 08:28:36 -0600

    Added loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 class App extends React.Component {
   state = {
     lat: null,
@@ -25,7 +26,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
